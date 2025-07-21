08830207184b74be13e23d54ba46c4c1fff94578
CiroGomess <ciro.ggj@gmail.com>
2025-07-21 19:26:52 -0300

    feat: config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lampys-maquina',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
